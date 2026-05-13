03f1897c62bb0aa6054a6489d69fe20d81ecdece
Flávia Paiva <SEU_EMAIL_DO_GITHUB>
2026-05-12 22:04:37 -0300

    feat: adiciona resumo QA dinâmico com JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("SPA Responsive QA carregada com sucesso!");
+
+const totalTestes = 3;
+const totalBugs = 2;
+const totalChecklist = 4;
+
+document.getElementById("total-testes").textContent = totalTestes;
+document.getElementById("total-bugs").textContent = totalBugs;
+document.getElementById("total-checklist").textContent = totalChecklist;
